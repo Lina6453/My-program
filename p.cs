3c57ffec95866c587054c0c6d804729ae58a3c7d
Lina6453 <angel.gabid@yandex.ru>
2022-05-03 10:59:00 +0300

    m2

--- p.cs
+++ p.cs
@@ -2,6 +2,7 @@ using Sistem;
 class Person
 {
     private string name;
+    const int c = 3;
     public Person (string nm)
     {
         name = nm;
@@ -10,9 +11,15 @@ class Person
     {
         return name;
     }
+    public int Dochod(int a)
+    {
+        return a * c;
+    }
     static void Main (string[], args)
     {
         Person p = new Person("David");
         Console.WriteLine(p.GetName);
+        Person x = new Person (Convert.ToInt32("34000"));
+        Console.WriteLine(x.Dochod);
     }
 }
